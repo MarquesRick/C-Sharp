740b133c268956511baa64436f8aa9e18644b609
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2019-10-19 15:56:25 -0300

    Commit Exercicio de Fixação

---
+++ RecapitulacaoDeLogica/ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritorio";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char sexo = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine($"Produtos\n{produto1}, cujo preço eh de: ${preco1:F2}\n{produto2}, cujo preço eh de: ${preco2:F2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro\n{idade} anos de idade, codigo {codigo} e gênero: {sexo}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida}\nArredondado(3 casas decimais): {medida:F3}\nSeparador decimal invariante culture:" + medida.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
--- RecapitulacaoDeLogica/SaidaDeDados/SaidaDeDados/Program.cs
+++ RecapitulacaoDeLogica/SaidaDeDados/SaidaDeDados/Program.cs
@@ -18,9 +18,10 @@ namespace SaidaDeDados
 
             Console.WriteLine();
             Console.WriteLine(x);
-            Console.WriteLine(x.ToString("F2"));
-            Console.WriteLine(x.ToString("F4"));
-            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(x.ToString("F2")); //Forma para delimitar duas casas decimais
+            Console.WriteLine(x.ToString("F4")); //Forma para delimitar quatro casas decimais
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); //Forma para desconsiderar formatação especifica do país
+                                                                               //(na qual sua maquina está rodando) usa o '.' ao invés da ','.
 
             Console.WriteLine();
             Console.WriteLine("RESULTADO = " + x);
@@ -29,6 +30,13 @@ namespace SaidaDeDados
 
             Console.WriteLine();
             Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            //Placehoder
+            Console.WriteLine("A paciente {0} tem {1} anos e seu saldo eh: {2:F2}", z, y, x); //{2:F2} forma para trazer a segunda variável delimitando as casas decimais. Essa forma não possui o globalization
+            //Interpolação
+            Console.WriteLine($"A paciente {z} tem {y} anos e seu saldo eh: {x:F2}"); //{x:F2} forma para trazer a segunda variável delimitando as casas decimais. Essa forma não possui o globalization
+            //Concatenação
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu saldo eh: " + x.ToString("F2", CultureInfo.InvariantCulture)); //Essa forma possui o globalization
 
             Console.ReadLine();
         }
