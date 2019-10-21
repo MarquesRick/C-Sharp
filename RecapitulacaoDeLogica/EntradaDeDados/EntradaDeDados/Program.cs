e5842fb31f5f4a9c66c1f77a5f032ecbebdc632e
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2019-10-20 22:07:02 -0300

    Commit do módulo 1 e primeira aula do módulo 2 (classes e atributos)

--- RecapitulacaoDeLogica/EntradaDeDados/EntradaDeDados/Program.cs
+++ RecapitulacaoDeLogica/EntradaDeDados/EntradaDeDados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace EntradaDeDados
 {
@@ -41,7 +42,26 @@ namespace EntradaDeDados
             double c = (double)b / a;
             Console.WriteLine(c);
 
+            Console.WriteLine("\n------------------------------Utilizando PARSE------------------------------\n");
 
+            int n1 = int.Parse(Console.ReadLine()); //Necessario para ser executado a função ReadLine
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou as variáveis:");
+            Console.WriteLine(n1); 
+            Console.WriteLine(ch); 
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture)); 
+            Console.WriteLine(nome); 
+            Console.WriteLine(sexo); 
+            Console.WriteLine(idade); 
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture)); 
         }
     }
 }
---
+++ RecapitulacaoDeLogica/EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Utilizando IF/ELSE----------");
+
+            double a, b, c, delta, x1, x2;
+
+            Console.WriteLine("Digite o valor de A, B e C separados por espaço...");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = b * b - 4 * a * c;
+
+            if (a == 0.0 || delta < 0.0)
+            {
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }   
+            else
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("X1 = " + x1.ToString("F4", CultureInfo.InvariantCulture));
+                Console.WriteLine("X2 = " + x2.ToString("F4", CultureInfo.InvariantCulture));
+            }
+
+
+            Console.WriteLine("----------Utilizando WHILE----------");
+
+            Console.Write("Digite um numero:");
+            double numero = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+            while(numero >= 0.0)
+            {
+                double raiz = Math.Sqrt(numero);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro numero:");
+                numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Numero Negativo...");
+
+            Console.WriteLine("----------Utilizando FOR----------");
+
+            Console.Write("Digite o numero de inteiros que voce irá digitar:");
+
+            int soma = 0;
+            int N = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= N; i++)
+            {
+                Console.Write("Valor #{0}:", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+
+        }
+    }
+}
