93335dca398863caaaf494dcbe35b2f31764b6e7
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-01-26 21:35:08 -0300

    Implementing simple search

---
+++ Projeto MVC/SalesWebMVC/SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        //Dependencia da classe
+        private readonly SalesWebMVCContext _context;
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller) //faz o join entre tabela seller
+                .Include(x => x.Seller.Department) //faz o join entre tabela department
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
