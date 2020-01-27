40a1d2021a85d256e4bf558475d9ee7672484305
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-01-26 22:58:11 -0300

    Grouping Implementation

--- Projeto MVC/SalesWebMVC/SalesWebMVC/Services/SalesRecordService.cs
+++ Projeto MVC/SalesWebMVC/SalesWebMVC/Services/SalesRecordService.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SalesWebMVC.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
@@ -35,5 +34,25 @@ namespace SalesWebMVC.Services
                 .ToListAsync();
         }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller) //faz o join entre tabela seller
+                .Include(x => x.Seller.Department) //faz o join entre tabela department
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+
     }
 }
+
