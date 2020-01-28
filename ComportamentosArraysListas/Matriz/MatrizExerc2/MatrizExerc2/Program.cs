5a0667cb08af9d57b02b1f6d5ed92688c3b9ca62
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-01-27 23:59:52 -0300

    Matriz exercicio 2

---
+++ ComportamentosArraysListas/Matriz/MatrizExerc2/MatrizExerc2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MatrizExerc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a dimensão da sua Matriz, separada por espaço:");
+            string[] line = Console.ReadLine().Split(' ');
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
+
+            int[,] mat = new int[m, n];
+
+            int count = 0;
+            for (int i = 0; i < m; i++)
+            {
+                count++;
+                Console.WriteLine($"{count}ª linha da Matriz:");
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Digite o numero a ser pesquisado em sua Matriz:");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
