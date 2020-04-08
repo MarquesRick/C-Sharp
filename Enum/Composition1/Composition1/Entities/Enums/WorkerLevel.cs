1ac0bdc5ea8fb5b91ebda291bf47e49f030270cd
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-04-08 01:03:11 -0300

    Enum and compositions

---
+++ Enum/Composition1/Composition1/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition1.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        Midlevel = 1,
+        Senior = 2
+    }
+}
---
+++ Enum/Enum1/Enum1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+namespace Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+        
+            Console.WriteLine(order);
+
+            String txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+
+            Console.WriteLine(os);
+        }
+    }
+}
