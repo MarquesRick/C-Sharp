317075c9057761f396f318a4f5a4f2f537439be8
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-05-06 23:32:05 -0300

    Heranca, Casting and polimorfism

---
+++ Enum/Composition3/Composition3/Composition3/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition3.Enums
+{
+        enum OrderStatus : int
+        {
+            PendingPayment = 0,
+            Processing = 1,
+            Shipped = 2,
+            Delivered = 3
+        }
+    
+}
