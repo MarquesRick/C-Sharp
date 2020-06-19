3dbedefe21eb1dc01caae01080e7c41b4cc955f4
HENRIQUE MARQUES <henrique.marques2@fatec.sp.gov.br>
2020-06-19 20:02:23 -0300

    Commit Interfaces and inheritance

---
+++ TrabalhandoArquivos/ExercicioFixacao/ExercicioFixacao/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+        public Product(string name, double unitPrice, int quantite)
+        {
+            Name = name;
+            UnitPrice = unitPrice;
+            Quantity = quantite;
+        }
+
+        public double Total()
+        {
+            return UnitPrice * Quantity;
+        }
+    }
+}
---
+++ TrabalhandoArquivos/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ExercicioFixacao.Entities;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file full path: ");
+            string sourceFilePatch = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePatch);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePatch);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
